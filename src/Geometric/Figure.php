422a93f8250260c95c964e9ab9361951aecd86c8
David M. Rivero G <davidmriverog@gmail.com>
2017-10-10 23:40:27 -0400

    drivero - creamos metodo abstracto para obtener el area

--- src/Geometric/Figure.php
+++ src/Geometric/Figure.php
@@ -5,10 +5,15 @@ namespace Geopagos\Geometric;
 /**
  * interface Figure
  *
- * @package Geometric;
+ * @package Geopagos\Geometric;
  * @author David Rivero <[<davidmriverog@gmail.com>]>
  */
 interface Figure
 {
-    //
+    /**
+     * Calculate Area Figure.
+     * 
+     * @return double
+     */
+    public function area();
 }
